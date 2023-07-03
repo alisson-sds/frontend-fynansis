5a90f088b13844bb504d2a5fba37a8ae5d3be4b2
Alisson <alisson.silveira@kbase.com.br>
2023-07-03 12:35:21 -0300

    first commit

---
+++ Fynansis/src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Home from './pages/Home.tsx'
+import './styles/global.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+)
---
+++ Fynansis/src/pages/Home.tsx
@@ -0,0 +1,17 @@
+function Home() {
+
+  return (
+    <>
+      
+      <h1>Login</h1>
+      <input type="text" placeholder="Login"></input>
+      <br/>
+      <input type="password" placeholder="Senha"></input>
+      <br/>
+      <button type="button">Entrar</button>
+      
+    </>
+  )
+}
+
+export default Home
