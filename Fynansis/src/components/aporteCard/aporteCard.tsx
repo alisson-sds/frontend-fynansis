62c4f59d5942208cd66de570f3efe058fca5110d
Alisson <alisson-sds@hotmail.com>
2023-09-18 12:54:28 -0300

    Alterando botão que expande aportes

--- Fynansis/src/components/aporteCard/aporteCard.tsx
+++ Fynansis/src/components/aporteCard/aporteCard.tsx
@@ -9,12 +9,12 @@ export const AportCard = ({
   numCotas,
   codAport,
   deleteAporteFunc,
-  detalheAporte = false
+  detalheAporte = false,
 }: AportData) => {
   function formatData(dataCompra: string): string {
     const date = parseInt(dataCompra, 10);
 
-    const data = new Date(date);    
+    const data = new Date(date);
 
     return data.toLocaleDateString("pt-BR");
   }
@@ -22,20 +22,39 @@ export const AportCard = ({
 
   return (
     <div className="AportCard">
-      <h2>R$ {valorCompra * numCotas}</h2>
-      <p>{formatData(dataCompra)}</p>
-      <div className="div-icons-aport">
-        <i className="fa-solid fa-pen"></i>
-        <i
-          className="fa-solid fa-trash"
-          onClick={() => deleteAporteFunc(codAport)}
-        ></i>
-        <i className="fa-solid fa-arrow-down" onClick={() => setAbreAporte(!abreAporte)}></i>        
-      </div>      
-      {abreAporte? 
-      (<p>aaaaa</p>)
-      :
-      (<p>bbbbb</p>)}      
+      {abreAporte ? (
+        <>
+          <h2>R$ {valorCompra * numCotas}</h2>
+          <p>{formatData(dataCompra)}</p>
+          <div className="div-icons-aport">
+            <i className="fa-solid fa-pen"></i>
+            <i
+              className="fa-solid fa-trash"
+              onClick={() => deleteAporteFunc(codAport)}
+            ></i>
+            <i
+              className="fa-solid fa-arrow-up"
+              onClick={() => setAbreAporte(!abreAporte)}
+            ></i>
+          </div>
+        </>
+      ) : (
+        <>
+          <h2>R$ {valorCompra * numCotas}</h2>
+          <p>{formatData(dataCompra)}</p>
+          <div className="div-icons-aport">
+            <i className="fa-solid fa-pen"></i>
+            <i
+              className="fa-solid fa-trash"
+              onClick={() => deleteAporteFunc(codAport)}
+            ></i>
+            <i
+              className="fa-solid fa-arrow-down"
+              onClick={() => setAbreAporte(!abreAporte)}
+            ></i>
+          </div>
+        </>
+      )}
     </div>
   );
 };
